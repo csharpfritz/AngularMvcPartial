800a7c5ac75ee8a5b5a1081462759c23a717a4e9
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2016-07-22 11:43:52 -0400

    Updated with Julio's changes

--- AngularMvcPartial/Controllers/HomeController.cs
+++ AngularMvcPartial/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AngularMvcPartial.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +14,36 @@ namespace AngularMvcPartial.Controllers
             return View();
         }
 
+		public ActionResult WorkingCode()
+		{
+			return View();
+		}
+
+		public ActionResult ClientSetup(int id)
+		{
+			return View("_MyPartial");
+		}
+
+		public JsonResult ClientInformation(int id)
+		{
+			var view = GetModel();
+			return Json(view, JsonRequestBehavior.AllowGet);
+		}
+
+		private ClientAccountSetupViewModel GetModel()
+		{
+			var result = new ClientAccountSetupViewModel();
+			result.TenantID = 1;
+			result.SiteProcessings = new List<SiteProcessing>() {
+				new SiteProcessing { SiteID = 1, SiteName ="Horsham", RegionName = "Pennsylvania", GatewayAPIKey = "1234", CreditCardProcessorID = "4312", ACHProcessorID = "4312 "},
+				new SiteProcessing { SiteID = 2, SiteName = "Philadelphia", RegionName = "Pennsylvania", GatewayAPIKey = null, CreditCardProcessorID = null, ACHProcessorID = null},
+				new SiteProcessing { SiteID = 3, SiteName = "Camden", RegionName = "New Jersey",  GatewayAPIKey = "4333", CreditCardProcessorID = "343", ACHProcessorID = null},
+				new SiteProcessing { SiteID = 4, SiteName = "Trenton", RegionName = "New Jersey", GatewayAPIKey = null, CreditCardProcessorID = "asd", ACHProcessorID = "dsad"},
+				new SiteProcessing { SiteID = 5, SiteName = "Wilmington", RegionName = "Delaware", GatewayAPIKey = "2231", CreditCardProcessorID = null, ACHProcessorID = "4433"},
+				new SiteProcessing { SiteID = 6, SiteName = "Dover", RegionName = "Delaware", GatewayAPIKey = null, CreditCardProcessorID = null, ACHProcessorID = null}
+			};
+
+			return result;
+		}
     }
 }
---
+++ AngularMvcPartial/Models/ClientAccountSetupViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularMvcPartial.Models
+{
+	public class ClientAccountSetupViewModel
+	{
+		public int TenantID { get; set; }
+
+		public List<SiteProcessing> SiteProcessings { get; set; }
+	}
+
+	public class SiteProcessing
+	{
+		public int SiteID { get; set; }
+		public string SiteName { get; set; }
+		public string RegionName { get; set; }
+		public string GatewayAPIKey { get; set; }
+		public string CreditCardProcessorID { get; set; }
+		public string ACHProcessorID { get; set; }
+	}
+}
